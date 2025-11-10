790d98a1f91b857103f4f11f434f87936ea5beb3
Pattharaphon Khajitsuwan <marknaja33240@gmail.com>
2025-11-10 16:06:43 +0700

    API + Frontend

---
+++ utils/db.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql2');
+
+export const mysqlPool = mysql.createPool({
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASS || "",
+  database: process.env.DB_NAME || 'u6703952_dit205',
+  port: Number(process.env.DB_PORT || 3306),
+  waitForConnections: true,
+  connectionLimit: 10,
+  ssl: process.env.DB_SSL === 'true' ? { 
+    minVersion: 'TLSv1.2', 
+    rejectUnauthorized: true 
+  } : undefined,
+});
